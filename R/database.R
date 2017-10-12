34500dd99778380488da914ab8a0c78e7fa026c2
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-10-12 15:32:57 +0100

    Argument name change host -> instance

--- R/database.R
+++ R/database.R
@@ -1,30 +1,32 @@
-database_connection <- function(instance = "science", user = "readonly",
+database_connection <- function(location = "science", user = "readonly",
                                 port = NULL) {
-  if (instance == "science") {
+  if (location == "science") {
     host <- "support.montagu.dide.ic.ac.uk"
     port <- 5432
-  } else if (instance == "uat") {
+  } else if (location == "uat") {
     host <- "support.montagu.dide.ic.ac.uk"
     port <- 15432
-  } else if (instance == "production") {
+  } else if (location == "production") {
     host <- "production.montagu.dide.ic.ac.uk"
     port <- 5432
-  } else if (instance == "localhost") {
+  } else if (location == "localhost") {
     host <- "localhost"
     port <- port %||% 5432
   } else {
-    stop("Unknown instance ", instance)
+    stop("Unknown location ", location)
   }
-  if (instance == "uat") {
+  if (location == "uat") {
     password <- "changeme"
   } else {
     password <- vault_read(sprintf("/secret/database/users/%s", user),
                            "password")
   }
-  DBI::dbConnect(RPostgres::Postgres(),
-                 dbname = "montagu",
-                 host = host,
-                 port = port,
-                 password = password,
-                 user = user)
+  ret <- DBI::dbConnect(RPostgres::Postgres(),
+                        dbname = "montagu",
+                        host = host,
+                        port = port,
+                        password = password,
+                        user = user)
+  attr(ret, "location") <- location
+  ret
 }
