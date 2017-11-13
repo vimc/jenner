85c4b47d7892e13033350007fcf3d0270d06ffba
Xiang Li <x.li@imperial.ac.uk>
2017-11-13 13:08:27 +0000

    revert RIGHT JOIN to LEFT JOIN. Update this bit when migration of MR_Measles is done.

--- inst/sql/modified_update/migrate_coverage.sql
+++ inst/sql/modified_update/migrate_coverage.sql
@@ -5,7 +5,7 @@ coverage_old.id AS coverage_set_id_old,
 coverage_new.id AS coverage_set_id_new
 FROM (SELECT * FROM coverage_set
       WHERE touchstone != '{{{touchstone_new}}}') AS coverage_old
-RIGHT JOIN (SELECT coverage_set.* FROM coverage_set
+LEFT JOIN (SELECT coverage_set.* FROM coverage_set
 JOIN touchstone ON touchstone.id = coverage_set.touchstone
            WHERE touchstone = '{{{touchstone_new}}}'
            AND (coverage_set.gavi_support_level = 'with'
