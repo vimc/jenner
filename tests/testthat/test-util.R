6a298c2f571e0ffa9486ccccda3f871ee4e05d88
Xiang Li <x.li@imperial.ac.uk>
2018-02-27 13:00:39 +0000

    roxygen

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -22,12 +22,6 @@ test_that("dquote", {
   expect_equal(dquote(c("a", "b")), c('"a"', '"b"'))
 })
 
-test_that("read_sql", {
-  file_read_by_function <- "impact_method2_metadata/group.sql"
-  file_read_by_path <- "../../inst/sql/impact_method2_metadata/group.sql"
-  expect_equal(read_sql(file_read_by_function), read_file(file_read_by_path))
-})
-
 test_that("merge_in", {
   dat <- data_frame(.code = 1:3, id=1:3, b = c("a", "b", "c"))
   d <- data_frame(.code = 1:3, B = c("A", "B", "C"))
