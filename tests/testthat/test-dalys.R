374016869c1c4f04035f8601d05a8cb17222a016
Xiang Li <x.li@imperial.ac.uk>
2018-03-14 14:06:07 +0000

    forgot to commit this change

--- tests/testthat/test-dalys.R
+++ tests/testthat/test-dalys.R
@@ -7,12 +7,26 @@ test_that("duration_weighting", {
 })
 
 test_that("dalys_calculation", {
+  skip_if_not_installed("RSQLite")
+  con <- test_montagu_readonly_connection()
+  con_test <- DBI::dbConnect(RSQLite::SQLite(), dbname=":memory:")
+  on.exit({
+    DBI::dbDisconnect(con)
+    DBI::dbDisconnect(con_test)
+  })
 
+  # parameters - we are testing against Measles - Jit
   dalys_src <- read.csv("dalys_parameters.csv", stringsAsFactors = FALSE)
+  touchstone_name <- "201710gavi"
+  year_min <- 2020
+  year_max <- 2020
+  stopifnot(unique(dalys_src$modelling_group) == "LSHTM-Jit")
+  # responsibility 328-MCV1 332-No vac
+  # burden_estimate 276-MCV1 275-No vac
   ##mannual calculation
-  a <- NULL
+  a <- DBI::dbGetQuery(con, "SELECT ")
   ## run function
-  dat <- jenner::calculate_dalys(con, dalys_src, touchstone_name, year_min, year_max,  vimc_dalys_only = TRUE)
+  dat <- jenner::calculate_dalys(con, dalys_src, touchstone_name, year_min, year_max)
 
   saveRDS(a, "jenner-test-data/dalys_calculation/Jit-Measles.rds")
   expect_known_value(a, "jenner-test-data/dalys/Jit-Measles.rds",
