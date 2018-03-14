5ea71b9b3fcf249ca04e131409c169b7a84148ec
Xiang Li <x.li@imperial.ac.uk>
2018-03-14 14:04:01 +0000

    testing is suspended, I am waiting for DOVE to confirm dalys parameters

--- tests/testthat/test-dalys.R
+++ tests/testthat/test-dalys.R
@@ -5,3 +5,16 @@ test_that("duration_weighting", {
   w <- c(1., 1./365, 1./12)
   expect_equal(duration_weighting(period), w)
 })
+
+test_that("dalys_calculation", {
+
+  dalys_src <- read.csv("dalys_parameters.csv", stringsAsFactors = FALSE)
+  ##mannual calculation
+  a <- NULL
+  ## run function
+  dat <- jenner::calculate_dalys(con, dalys_src, touchstone_name, year_min, year_max,  vimc_dalys_only = TRUE)
+
+  saveRDS(a, "jenner-test-data/dalys_calculation/Jit-Measles.rds")
+  expect_known_value(a, "jenner-test-data/dalys/Jit-Measles.rds",
+                     update = FALSE)
+})
