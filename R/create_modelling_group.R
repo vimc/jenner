0ba20829ab0ec829ca371b3d2576c1da43592682
Xiang Li <x.li@imperial.ac.uk>
2018-05-03 14:31:25 +0100

    setup plan, do the work when we have a new candidate

---
+++ R/create_modelling_group.R
@@ -0,0 +1,3 @@
+## Ideally, we need two functions
+# create_modelling_group: need a csv meta that includes - disease/group name/institution/pi/description/model/model_description/citation
+# assign_responsibility: need disease/touchstone/group name
